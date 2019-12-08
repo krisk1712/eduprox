010cf041ccce6469745481daf1d42c2c78c350f8
Kannan <Kannan@Kannans-MacBook-Air.local>
2019-12-08 23:07:18 +0530

    make it a

---
+++ app.js
@@ -0,0 +1,41 @@
+/***************
+ * node-unblocker: Web Proxy for evading firewalls and content filters,
+ * similar to CGIProxy or PHProxy
+ *
+ *
+ * This project is hosted on github:  https://github.com/nfriedly/node-unblocker
+ *
+ * By Nathan Friedly - http://nfriedly.com
+ * Released under the terms of the GPL v3
+ */
+
+var url = require('url');
+var querystring = require('querystring');
+var express = require('express');
+var unblocker = require('unblocker');
+var Transform = require('stream').Transform;
+
+var app = express();
+
+var unblockerConfig = {
+    prefix: '/proxy/'
+};
+
+
+
+// this line must appear before any express.static calls (or anything else that sends responses)
+app.use(unblocker(unblockerConfig));
+
+// serve up static files *after* the proxy is run
+app.use('/', express.static(__dirname + '/public'));
+
+// this is for users who's form actually submitted due to JS being disabled or whatever
+app.get("/no-js", function(req, res) {
+    // grab the "url" parameter from the querystring
+    var site = querystring.parse(url.parse(req.url).query).url;
+    // and redirect the user to /proxy/url
+    res.redirect(unblockerConfig.prefix + site);
+});
+
+// for compatibility with gatlin and other servers, export the app rather than passing it directly to http.createServer
+module.exports = app;
